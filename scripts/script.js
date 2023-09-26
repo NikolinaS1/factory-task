1dedc8056dd0603669b3e57af282d055c2f34b7f
DESKTOP-VR8NC81\Nikolina <nikolina.stjepanovic@student.ferit.hr>
2023-09-26 15:46:00 +0200

    slider fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
   const leftButton = $(".left");
   const rightButton = $(".right");
+  let isAnimating = false;
 
   const disableButtons = () => {
     leftButton.prop("disabled", true);
@@ -12,29 +13,77 @@ $(document).ready(() => {
     rightButton.prop("disabled", false);
   };
 
-  leftButton.click(() => {
-    disableButtons();
-    const currentTopImage = $("#top-row .image:first-child");
-    const currentBottomImage = $("#bottom-row .image:first-child");
-    currentTopImage
-      .appendTo("#top-row")
-      .hide()
-      .fadeIn(500, () => {
+  leftButton.on("click", () => {
+    if (!isAnimating) {
+      isAnimating = true;
+      disableButtons();
+
+      const topRow = $("#top-row");
+      const bottomRow = $("#bottom-row");
+
+      const topRowImageWidth = topRow.find(".image:first-child").outerWidth();
+      const bottomRowImageWidth = bottomRow
+        .find(".image:first-child")
+        .outerWidth();
+
+      topRow.css("transition", "transform 0.5s ease-in-out");
+      bottomRow.css("transition", "transform 0.5s ease-in-out");
+      topRow.css("transform", `translateX(-${topRowImageWidth}px)`);
+      bottomRow.css("transform", `translateX(-${bottomRowImageWidth}px)`);
+
+      setTimeout(() => {
+        topRow.find(".image:first-child").appendTo(topRow).hide().fadeIn(500);
+        bottomRow
+          .find(".image:first-child")
+          .appendTo(bottomRow)
+          .hide()
+          .fadeIn(500);
+
+        topRow.css("transform", "translateX(0)");
+        bottomRow.css("transform", "translateX(0)");
+        topRow.css("transition", "none");
+        bottomRow.css("transition", "none");
+
         enableButtons();
-      });
-    currentBottomImage.appendTo("#bottom-row").hide().fadeIn(500);
+        isAnimating = false;
+      }, 500);
+    }
   });
 
-  rightButton.click(() => {
-    disableButtons();
-    const currentTopImage = $("#top-row .image:last-child");
-    const currentBottomImage = $("#bottom-row .image:last-child");
-    currentTopImage
-      .prependTo("#top-row")
-      .hide()
-      .fadeIn(500, () => {
+  rightButton.on("click", () => {
+    if (!isAnimating) {
+      isAnimating = true;
+      disableButtons();
+
+      const topRow = $("#top-row");
+      const bottomRow = $("#bottom-row");
+
+      const topRowImageWidth = topRow.find(".image:last-child").outerWidth();
+      const bottomRowImageWidth = bottomRow
+        .find(".image:last-child")
+        .outerWidth();
+
+      topRow.css("transition", "transform 0.5s ease-in-out");
+      bottomRow.css("transition", "transform 0.5s ease-in-out");
+      topRow.css("transform", `translateX(${topRowImageWidth}px)`);
+      bottomRow.css("transform", `translateX(${bottomRowImageWidth}px)`);
+
+      setTimeout(() => {
+        topRow.find(".image:last-child").prependTo(topRow).hide().fadeIn(500);
+        bottomRow
+          .find(".image:last-child")
+          .prependTo(bottomRow)
+          .hide()
+          .fadeIn(500);
+
+        topRow.css("transform", "translateX(0)");
+        bottomRow.css("transform", "translateX(0)");
+        topRow.css("transition", "none");
+        bottomRow.css("transition", "none");
+
         enableButtons();
-      });
-    currentBottomImage.prependTo("#bottom-row").hide().fadeIn(500);
+        isAnimating = false;
+      }, 500);
+    }
   });
 });
